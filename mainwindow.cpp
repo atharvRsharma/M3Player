b41e500a4f211b050b7f6ca8cfc9b8483aeadad8
atharv sharma <atharv1908@gmail.com>
2025-11-26 18:07:48 +0530

    drag and drop functionality fully added

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,57 +1,52 @@
 #include "mainwindow.h"
-#include "qaudiooutput.h"
 #include "ui_mainwindow.h"
-
 #include <QFileDialog>
-#include <QString>
-#include <QFile>
-#include <QTextStream>
 #include <QtWidgets>
-#include <QDebug>
-
-
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    // defns
+    setAcceptDrops(true);
+
+
     player = new QMediaPlayer(this);
     audio = new QAudioOutput(this);
     video = new QVideoWidget(this);
-    slider = new QSlider(this);
-    bar = new QProgressBar(this);
 
-    slider->setOrientation(Qt::Horizontal);
-    ui->statusBar->addPermanentWidget(slider);
-    ui->statusBar->addPermanentWidget(bar);
 
-    // resizing
-    QVBoxLayout *layout = new QVBoxLayout();
-    layout->addWidget(video);
+    dropZone = new QLabel(this);
+    dropZone->setAcceptDrops(true);
+
+    dropZone->setStyleSheet("background-color: transparent;");
+
+    dropZone->installEventFilter(this);
+
+
+    QStackedLayout *stack = new QStackedLayout();
+    stack->setStackingMode(QStackedLayout::StackAll);
+
+    stack->addWidget(video);
+    stack->addWidget(dropZone);
+
     QWidget *container = new QWidget(this);
-    container->setLayout(layout);
+    container->setLayout(stack);
     setCentralWidget(container);
 
 
+    player->setAudioOutput(audio);
+    player->setVideoOutput(video);
+    audio->setVolume(1.0);
+
+    slider = new QSlider(Qt::Horizontal, this);
+    //slider->setFixedWidth(400);
+    ui->statusBar->addPermanentWidget(slider, Qt::AlignHCenter);
 
 
-
-    // connect signals to slots, preferred the old SIGNAL SLOT stuff lwk
     connect(player, &QMediaPlayer::durationChanged, slider, &QSlider::setMaximum);
     connect(player, &QMediaPlayer::positionChanged, slider, &QSlider::setValue);
     connect(slider, &QSlider::sliderMoved, player, &QMediaPlayer::setPosition);
-    //connect(slider, &QSlider::sliderPressed, player, &QMediaPlayer::setPosition);
-
-    //connect(player, &QMediaPlayer::durationChanged, bar, &QProgressBar::setMaximum);
-    //connect(player, &QMediaPlayer::positionChanged, bar, &QProgressBar::setValue);
-
-    // giving the player the essentials
-    player->setAudioOutput(audio);
-    player->setVideoOutput(video);
-
-    video->show();
 }
 
 MainWindow::~MainWindow()
@@ -59,47 +54,65 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_actionOpen_triggered()
-{
-    fileNames = QFileDialog::getOpenFileNames(
-        this,
-        tr("Select Media File"),
-        QDir::homePath(),
-        tr("Media Files (*.mp4 *.mp3 *.wav *.avi *.mkv);;All Files (*.*)")
-        );
 
-    if (!fileNames.isEmpty()) {
-        for(uint32_t i = 0; i < fileNames.size(); ++i){
-            player->setSource(QUrl::fromLocalFile(fileNames[i]));
-            player->play();
+void MainWindow::dropEvent(QDropEvent *e)
+{
+    foreach (const QUrl &url, e->mimeData()->urls()) {
+        QString fileName = url.toLocalFile();
+        if (fileName.isEmpty()) return;
+
+        ui->statusBar->showMessage("Playing: " + fileName);
+        player->setSource(QUrl::fromLocalFile(fileName));
+        player->play();
+    }
+}
+
+void MainWindow::dragEnterEvent(QDragEnterEvent *e)
+{
+    if (e->mimeData()->hasUrls()) {
+        e->acceptProposedAction();
+    }
+}
+
+
+bool MainWindow::eventFilter(QObject *watched, QEvent *event)
+{
+
+    if (watched == dropZone) {
+
+        if (event->type() == QEvent::DragEnter) {
+            QDragEnterEvent *dragEvt = static_cast<QDragEnterEvent*>(event);
+            dragEvt->acceptProposedAction();
+            return true;
+        }
+
+        if (event->type() == QEvent::Drop) {
+            QDropEvent *dropEvt = static_cast<QDropEvent*>(event);
+
+            MainWindow::dropEvent(dropEvt);
+            return true;
         }
     }
 
-    fileNames.clear();
+    return QMainWindow::eventFilter(watched, event);
 }
 
 
-void MainWindow::on_actionStop_triggered()
+void MainWindow::on_actionStop_triggered() { player->stop(); ui->statusBar->showMessage("stopped"); }
+void MainWindow::on_actionPlay_triggered() { player->play(); ui->statusBar->showMessage("playing"); }
+void MainWindow::on_actionPause_triggered() { player->pause(); ui->statusBar->showMessage("paused"); }
+
+void MainWindow::on_actionOpen_triggered()
 {
-    player->stop();
-    ui->statusBar->showMessage("stopped, file instance removed");
+    QStringList files = QFileDialog::getOpenFileNames(this, tr("Select Media"), QDir::homePath());
+    if (!files.isEmpty()) {
+        player->setSource(QUrl::fromLocalFile(files.first()));
+        on_actionPlay_triggered();
+    }
 }
 
-
-
-void MainWindow::on_actionPlay_triggered()
+void MainWindow::on_actionNext_triggered()
 {
-    player->play();
-    ui->statusBar->showMessage("playing");
+
 }
-
-
-void MainWindow::on_actionPause_triggered()
-{
-    player->pause();
-    ui->statusBar->showMessage("paused");
-}
-
-
-
 
--- mainwindow.h
+++ mainwindow.h
@@ -3,16 +3,17 @@
 
 #include <QMainWindow>
 #include <QMediaPlayer>
-#include <QApplication>
-#include <QSlider>
-#include <QProgressBar>
 #include <QAudioOutput>
 #include <QVideoWidget>
+#include <QSlider>
+#include <QDragEnterEvent>
+#include <QDropEvent>
+#include <QMimeData>
+#include <QLabel>
+#include <QStackedLayout>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -23,23 +24,28 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void dropEvent(QDropEvent *e) override;
+    void dragEnterEvent(QDragEnterEvent *e) override;
+
+
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
 private slots:
     void on_actionOpen_triggered();
-
     void on_actionStop_triggered();
-
     void on_actionPlay_triggered();
-
     void on_actionPause_triggered();
 
+    void on_actionNext_triggered();
 
 private:
     Ui::MainWindow *ui;
     QMediaPlayer *player;
     QAudioOutput *audio;
     QVideoWidget *video;
+
+    QLabel *dropZone;
     QSlider *slider;
-    QProgressBar *bar;
-    QStringList fileNames;
 };
-#endif // MAINWINDOW_H
+#endif
